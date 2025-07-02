2cf33a45b7cc3067d25d7089fa1ae0af9b5a75b9
myconstructivismmess <rouetaubrey@hyperlensed.com>
2025-07-02 17:46:59 +0200

    Add BuisList receptors/transmitters base logic (WIP)

--- Nodes/Logic/List/BuisList.cs
+++ Nodes/Logic/List/BuisList.cs
@@ -10,6 +10,13 @@ namespace BuisSystem.Nodes.Logic.List {
 #region Properties
 
 		protected static string StartListPropertyName => "List";
+		protected static string StartListIndexPropertyName => $"{StartListPropertyName}.Index";
+		protected static string StartListItemPropertyName => $"{StartListPropertyName}.Item";
+		
+
+
+		[ExportGroup("Receptors")]
+
 		[Export]
 		public string ListPropertyName {
 			get {
@@ -20,17 +27,71 @@ namespace BuisSystem.Nodes.Logic.List {
 			}
 		}
 
+
+
+		[ExportGroup("Transmitters")]
+
+		[Export]
+		public string ListItemPropertyName {
+			get {
+				return _listItemPropertyTransmitter.Name;
+			}
+			set {
+				_listItemPropertyTransmitter.Name = value;
+			}
+		}
+
+		[Export]
+		public bool ListIndexPropertyEnabled {
+			get {
+				return _listIndexPropertyTransmitter != null;
+			}
+			set {
+				if (value == ListIndexPropertyEnabled) {
+					return;
+				}
+
+				_listIndexPropertyTransmitter = value
+					? _core.CreatePropertyTransmitter(
+						_listIndexPropertyName
+					)
+					: null;
+			}
+		}
+		
+		private string _listIndexPropertyName = StartListIndexPropertyName;
+		[Export]
+		public string ListIndexPropertyName {
+			get {
+				return _listIndexPropertyName;
+			}
+			set {
+				_listIndexPropertyName = value;
+
+				if (_listIndexPropertyTransmitter != null) {
+					_listIndexPropertyTransmitter.Name = value;
+				}
+			}
+		}
+
 #endregion
 
 #region Property Receptors
 
-		protected readonly IPropertyReceptor _listPropertyReceptor;
+		private readonly IPropertyReceptor _listPropertyReceptor;
 		
+#endregion
+
+#region Property Transmitters
+
+		private readonly IPropertyTransmitter _listItemPropertyTransmitter;
+		private IPropertyTransmitter _listIndexPropertyTransmitter;
+
 #endregion
 
 #region Buis Core Bridge
 
-		protected BuisComponentCore _core = new BuisComponentCore();
+		protected readonly BuisComponentCore _core = new BuisComponentCore();
 
 		public GodotNodeWeakRefWrapper<IBuisComponent> ParentComponent => _core.ParentComponent;
 
@@ -47,7 +108,12 @@ namespace BuisSystem.Nodes.Logic.List {
 			_listPropertyReceptor = _core.CreatePropertyReceptor(
 				StartListPropertyName
 			);
-		}
 
+			// Property Transmitters
+			_listItemPropertyTransmitter = _core.CreatePropertyTransmitter(
+				StartListItemPropertyName
+			);
+			_listIndexPropertyTransmitter = null;
+		}
 	}
 }
