cfa66c8afe7bbed83c51a46beed02dee81a3fcf2
myconstructivismmess <rouetaubrey@hyperlensed.com>
2025-07-02 08:56:29 +0200

    Update BuisList to add the receptor

--- Nodes/Logic/List/BuisList.cs
+++ Nodes/Logic/List/BuisList.cs
@@ -1,23 +1,53 @@
 using Godot;
+
+using BuisSystem.Core;
 using BuisSystem.Nodes.Core;
 
 namespace BuisSystem.Nodes.Logic.List {
 	[Tool]
 	public partial class BuisList : Node, IBuisComponent {
+
+#region Properties
+
+		protected static string StartListPropertyName => "List";
+		[Export]
+		public string ListPropertyName {
+			get {
+				return _listPropertyReceptor.Name;
+			}
+			set {
+				_listPropertyReceptor.Name = value;
+			}
+		}
+
+#endregion
+
+#region Property Receptors
+
+		protected readonly IPropertyReceptor _listPropertyReceptor;
 		
-#region Buis Component Core Bridge
+#endregion
+
+#region Buis Core Bridge
 
 		protected BuisComponentCore _core = new BuisComponentCore();
 
-		public IBuisComponent ParentComponent => _core.ParentComponent;
+		public GodotNodeWeakRefWrapper<IBuisComponent> ParentComponent => _core.ParentComponent;
 
 		public sealed override void _Notification(int what) {
 			OnNotification(what);
-			_core.OnNotification(this, what);
+			_core.OnNotification(this, this, what);
 		}
 		protected virtual void OnNotification(int what) {}
 
 #endregion
+
+		public BuisList() {
+			// Property Receptors
+			_listPropertyReceptor = _core.CreatePropertyReceptor(
+				StartListPropertyName
+			);
+		}
 
 	}
 }
